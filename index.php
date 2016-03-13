cb73017f9db81eb6195e42978bf204fe64536216
robij9528 <jakerobinson91@outlook.com>
2016-03-12 23:59:35 -0500

    changed index to new content

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <meta charset="utf-8">
 <title>Eating Disorder Foundation of Canada</title>
 <link href="css/main.css" rel="stylesheet" type="text/css" media="screen"> 
-<link rel="stylesheet" href="css/foundation.css" />
+<link rel="stylesheet" href="css/foundation.css"/>
 <link href='https://fonts.googleapis.com/css?family=Lato:400,700,300,900' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300' rel='stylesheet' type='text/css'>
 </head>
@@ -30,85 +30,63 @@
 	include('includes/nav.html'); 
 ?>
 
-<div class="bigImage">
-	<img src="images/video.jpg" alt="video Placeholder">
-</div>
-<section class="row">
-<div class="large-12 small-12 textOnLite columns">
-<h2>EDFC IS AN NON-PROFIT ORGANIZATION TO HELP THOSE WITH AN EATING DISORDER RECOVER.
-</h2>
-<p>The Eating Disorders Foundation of Canada is enthusiastically behind local community groups that have set up awareness campaigns, support groups, education in the schools and much more, and sees a leadership role in bringing these local initiatives together to give them a stronger voice when it comes to provincial and federal funding. Governments are looking for collaboration, and with communities from across the country working together, eating disorders can have an enhanced national presence. Having an eating disorder is not a choice, but a severe and debilitating mental illness and the toll eating disorders have played on young women and their families is immeasurable. To this end the Eating Disorders Foundation of Canada acknowledges the need for urgent attention and is committed to raising funds to be directed toward the establishment of residential treatment, transition housing and to support ongoing research and treatment programs for eating disorders.</p>	
-</div>
-
-</section>
-
-<div class="topBannerFoun full-width text-center">
-	<div class="sideLink">
-
-	<img src="images/link1.jpg">
-	</div>
-	<div class="centreLink">
-
-	<img src="images/link2.jpg">
-	</div>
-	<div class="sideLink">
-
-	<img src="images/link3.jpg">
-	</div>
+<!-- <div class="bigImage">
 	
+</div> -->
+
+<div class="row">
+	<div class="splitImage">
+		<img class="small-8 large-8 columns" src="images/split_image1.png" alt="two faces">
+		<div class="small-4 large-4 end columns splitText">
+			<div class="splitTextpadding">
+				<h2>EDFC wants to HELP those with an eating Disorder</h2>				
+				<p>The Eating Disorders Foundation of Canada is enthusiastically behind local community groups that have set up awareness campaigns, support groups, education in the schools and much more, and sees a leadership role in bringing these local initiatives together to give them a stronger voice when it comes to provincial and federal funding. Governments are looking for collaboration, and with communities from across the country working together, eating disorders can have an enhanced national presence. Having an eating disorder is not a choice.</p>
+			</div>
+		</div>
+	</div>
 </div>
 
 
 <!-- PAST AND CURRENT EVENT LISTINGS ================= -->
-<section class="row textOnLite">
-	<div class="large-6 medium-6 small-12 columns">
-		<h2>Past Events</h2>
-		<div class="event">
-				<h6>Hope's Garden Fundraiser (Friday, November 20, 2015, 9pm)</h6> 
-		<p>Please join the Eating Disorders Foundation in collaboration with Hopes Garden to enjoy an evening of entertainment by the Stephanie McIlroy band at Norma Jeans.</p>
-		<a class="text-right" href="#">More...</a>
-		</div>
-		<div class="event">
-				<h6>A Summer Night's Dream - Benefit Concert for the Eating Disorders Foundation of Canada.</h6> 
-		<p>Join the Univox Choir, directed by Dallas Bergen, with speical guests Don Campbell and High School Sweetheart for a night of music of all genres. The concert will take place at Aeolian Hall on Sunday, June 24th at 7pm.</p>
-		<a class="text-right" href="#">More...</a>
-		</div>
-		<div class="event">
-				<h6>Eating Disorder Awareness Week (February 6-10, 2012)</h6>
-		<p>The Eating Disorders Foundation of Canada (EDF) is proud to announce that they were a platinum sponsor of the Hope’s Garden Eating Disorder Awareness Week annual breakfast, a highly successful initiative.
-</p>
-		<a class="text-right" href="#">More...</a>
-		</div>
-	</div>
+<section class="row">
+	<div class="small-12 large-12 columns upcomingEvents">
+		<div class="row">
+			<div class=" events small-12 large-6 columns">
+				<h2>Past Events</h2>
+				<img src="images/image6.jpg" alt="event">
+				<p>Hope's Garden Fundraiser (Friday, November 20, 2015, 9pm)
+				Please join the Eating Disorders Foundation in collaboration with Hopes Garden to enjoy an evening of entertainment by the Stephanie McIlroy band at Norma Jeans.</p>
+				<a href="#">Read More</a>
 
+				<img src="images/image6.jpg" alt="event">
+				<p>Hope's Garden Fundraiser (Friday, November 20, 2015, 9pm)
+				Please join the Eating Disorders Foundation in collaboration with Hopes Garden to enjoy an evening of entertainment by the Stephanie McIlroy band at Norma Jeans.</p>
+				<a href="#">Read More</a>
+			</div>
+			<div class="events small-12 large-6 columns">
+				<h2>Upcoming Events</h2>
+				<img src="images/image6.jpg" alt="event">
+				<p>Hope's Garden Fundraiser (Friday, November 20, 2015, 9pm)
+				Please join the Eating Disorders Foundation in collaboration with Hopes Garden to enjoy an evening of entertainment by the Stephanie McIlroy band at Norma Jeans.</p>
+				<a href="#">Read More</a>
 
-	<div class="large-6 medium-6 small-12 columns">
-		<h2>Upcoming Events</h2>
-		<div class="event">
-				<h6>February 1-7, 2016 is Eating Disorder Awareness Week 2016!</h6>
-		<p>Join Eating Disorders Foundation of Canada and organizations across Canada as we participate in Eating Disorders Awareness Week.</p>
-		<a class="text-right" href="#">More...</a>
-		</div>
-		<div class="event">
-				<h6>This summer is Eating Disorder Awareness Week 2016!</h6> 
-		<p>The Eating Disorders Foundation of Canada is proud to announce that they were in attendance at this exceptional event and plan to make a contribution to this most worthwhile cause. Accolades go to Meryl Bear for a very inspiring presentation.</p>
-		<a class="text-right" href="#">More...</a>
-		</div>
-		<div class="event">
-				<h6>Eating Disorder Awareness Month of August 9-12, 2016!</h6> 
-		<p>The EDF’s Fundraising Goals were announced. The value of Community Partnerships and the EDF’s initiatives were graciously acknowledged by Dr. Beth Mitchell, Director of Mental Health Services at London Health Sciences Centre and by Ross Collishaw, Chairman of the Board of WOTCH Housing.</p>
-		<a class="text-right" href="#">More...</a>
+				<img src="images/image6.jpg" alt="event">
+				<p>Hope's Garden Fundraiser (Friday, November 20, 2015, 9pm)
+				Please join the Eating Disorders Foundation in collaboration with Hopes Garden to enjoy an evening of entertainment by the Stephanie McIlroy band at Norma Jeans.</p>
+				<a href="#">Read More</a>
+
+			</div>
 		</div>
 	</div>
 </section>
 
 <section class="darkBg">
 	<div class="row">
-		<div class="large-12 small-12 columns textOnDark">
+		<div class="large-12 small-12 columns textOnDark text-center">
 			<h2>Fundraising Goals</h2>
 			<p>The Eating Disorders Foundation of Canada’s fundraising goal is to raise $3 million with allocations for the residential treatment facility and transition housing, technology, clinical evaluation and research, and support services.</p>
 		</div>
-		<div class="large-12 small-12 columns textOnDark">
+		<div class="large-12 small-12 columns textOnDark text-center">
 			<h2>Earmark Donations</h2>
 			<p>All donations can be earmarked for one of the four areas, all of which are vitally important. You may have a particular interest in one or more of the areas and the specific area of donations can be acknowledged and earmarked either via cheque or credit card. Please refer to our <a href="donations.php">donation’s page</a> for further information.</p>
 		</div>
@@ -116,29 +94,36 @@
 </section>
 <section class="creamBg" id="contactPage">
 	<div class="desktop leftImage">
-		<img src="images/university.jpg">
+		<!-- <img src="images/university.jpg"> -->
 	</div>
 	<div class="contactInfo textOnLite">
 		<h2>Contact Us</h2><br>
 			<form>
-			<input type="text"  id="donorName" placeholder="Name:">
-						<input type="email" id="donorAddress" placeholder="Email Address:">
-						<label>Subject</label>
-						<select>
-							<option>Business</option>
-							<option>Personal</option>
-						</select>
-						<label class="hide">
-							<input name="street" type="email" placeholder="street">
-						</label>
-						<textarea rows="6" id="donorComments" placeholder="Your Message:"></textarea>
-						<input type="submit" id="submitBtn" value="Submit">
+				<input type="text"  id="donorName" placeholder="Name:">
+				<input type="email" id="donorAddress" placeholder="Email Address:">
+				<label>Subject</label>
+				<select>
+					<option>Business</option>
+					<option>Personal</option>
+				</select>
+				<label class="hide">
+					<input name="street" type="email" placeholder="street">
+				</label>
+				<textarea rows="6" id="donorComments" placeholder="Your Message:"></textarea>
+				<input type="submit" id="submitBtn" value="Submit">
+			</form>
+	
+	<p>Eating Disorders Foundation of Canada<br>
+	Suite 230A, 100 Collip Circle<br>
+	Research Park, Western University<br> 
+	London ON, N6G 4X8<br>
 
-						</form>
-	
-	
-	<!-- <h4>Volunteering</h4>
-	<p>If you are interested in finding out about volunteer opportunities, please call or email the foundation for more information.</p> -->
+	Phone:  519- 858-5111<br>
+	Fax:  (519) 858-5086<br>
+	Email:  info@edfofcanada.com<br>
+	</p>
+	<h4>Volunteering</h4>
+	<p>If you are interested in finding out about volunteer opportunities, please call or email the foundation for more information.</p>
 	</div>
 	<div class="row"></div>
 </section>
