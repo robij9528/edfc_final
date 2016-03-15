5e3971a063cdc07968886bdfb57849a60807bfa0
Benjamin Smith <benjaminsmith@Benjamins-MacBook-Pro.local>
2016-03-14 22:06:02 -0400

    started working on javascript for nav, still needs some figuring out

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+  var scrollingNav = document.querySelector('.navBgScroll');
+var scrollable = document.querySelector('body');
 
 function toggleCP(){
 	var cp = document.getElementById("cp");
@@ -7,4 +9,27 @@ function toggleCP(){
 	} else {
 		cp.style.left = "0px";
 	}
+}
+
+scrollable.onscroll = function() {
+	changeNav(scrollingNav);
+};
+
+function changeNav(scrollingNav) {
+	
+
+	var elementHeight = scrollable.offsetHeight;
+	var scrollTopPosition = scrollable.scrollTop;
+	var scrollBottomPosition = scrollTopPosition + elementHeight;
+	var staticNav = document.querySelector('.navBg');
+	var staticNavVisible = (staticNav.offsetTop) >= scrollTopPosition;
+
+
+scrollingNav.style.top = scrollTopPosition + 'px';
+
+if (staticNavVisible) {
+	scrollingNav.style.display = 'block';
+}
+
+
 }
