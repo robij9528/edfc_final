55bf843fb481c6fe8c7b1a7044b7889519f3da71
robij9528 <Jake@Jake-Robinsons-MacBook.local>
2016-03-21 22:06:08 -0400

    added map

--- index.php
+++ index.php
@@ -138,6 +138,14 @@
 	</div>
 </section>
 
+<div class="row">
+	<div class="small-12 large-12 columns mapCont">
+		<h2>Visit Us</h2>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2917.241511577122!2d-81.28244088505049!3d43.01530347914837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882eee4761f9972d%3A0x25cf884139027f1b!2sResearch+Park!5e0!3m2!1sen!2sca!4v1458611048767" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	</div>
+</div>
+
+
 <section class="row" id="contactPage">
 	<div class="small-12 large-6 columns contactInfo textOnLite">
 		<h2>Contact Us</h2><br>
